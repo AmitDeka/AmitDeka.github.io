30424b0129dd6e9ef83186b54cbe0d33dd02e0be
AmitDeka <amitdeka49@gmail.com>
2021-07-03 23:05:39 +0530

    added working contact form

--- assets/js/index.js
+++ assets/js/index.js
@@ -64,3 +64,28 @@ tabs.forEach(tab => {
         tab.classList.add('qualification__active')
     })
 })
+
+// contact form submit
+
+var form = document.getElementById("contact-form");
+    
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var status = document.getElementById("status");
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        status.classList.add("success");
+        status.innerHTML = "Thank you for getting in touch!";
+        form.reset();
+      }).catch(error => {
+        status.classList.add("error");
+        status.innerHTML = "Oops! We have encountered a problem."
+      });
+    }
+    form.addEventListener("submit", handleSubmit)
